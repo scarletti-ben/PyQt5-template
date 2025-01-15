bb98280f8c40be4fe258768e1f74d6b3d6d16ae4
scarletti.ben@gmail.com <scarletti.ben@gmail.com>
2025-01-15 07:00:42 +0000

    Added Google Material icon and associated resource path

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -12,6 +12,7 @@ import json
 class Settings:
 
     TITLE: str = "PyQt5-template"
+    ICON: str = "resources/icons/material-128x128.ico"
     X: int = 100
     Y: int = 100
     W: int = 720
@@ -23,8 +24,9 @@ class Settings:
 
         with open("app/utils/settings.json", 'r') as f:
             settings = json.load(f)
-            cls.TITLE = settings.get("TITLE", "PyQt5-template")
-            cls.X = settings.get("X", 100)
-            cls.Y = settings.get("Y", 100)
-            cls.W = settings.get("W", 720)
-            cls.H = settings.get("H", 480)
+            cls.TITLE = settings.get("TITLE", cls.TITLE)
+            cls.ICON = settings.get("ICON", cls.ICON)
+            cls.X = settings.get("X", cls.X)
+            cls.Y = settings.get("Y", cls.Y)
+            cls.W = settings.get("W", cls.W)
+            cls.H = settings.get("H", cls.H)
